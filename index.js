91825d7aab805de21cb9d4e1553ff9110ce55225
Adam Paulukanis <adam@Adams-MacBook-Pro.local>
2019-06-24 19:30:57 +0100

    Add new life form: WallFollowers
    There is, now, a new critter that keeps it left hand to the wall and
    follows along.
    
    To implement this, the author of the book came up with `dirPlus`
    function which calculates relative directions. So `dirPlus('n', 1)`
    means one 45-degree turn clockwise from north, giving 'ne'.
    
    Have a look at the example *plan* world and *~* creature.
    
    TODO: implement A More Lifelike Simulation
    https://eloquentjavascript.net/2nd_edition/07_elife.html#h_yTrS5ee3KB

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 const plan = [
   '############################',
-  '#      #      #            #',
-  '#oooooooooooooooooooooo    #',
+  '# ~~~  #      #            #',
+  '#                     o    #',
   '#           #####          #',
   '##          # o #    ##    #',
   '###            ##     #    #',
@@ -205,7 +205,58 @@ View.prototype.find = function (ch) {
   return randomElement(found)
 }
 
-let world = new World(plan, { '#': Wall, 'o': BouncingCritter })
+/**
+ * Since directions are modeled by a set of strings, we need to define our own operation (dirPlus)
+ * to calculate relative directions. So dirPlus("n", 1) means one 45-degree turn clockwise from north,
+ * giving "ne". Similarly, dirPlus("s", -2) means 90 degrees counterclockwise from south, which is east.
+ */
+function dirPlus (dir, n) {
+  const index = directionNames.indexOf(dir)
+  return directionNames[(index + n + 8) % 8]
+}
+
+/**
+ * There is a critter that moves along walls.
+ * Conceptually, the critter keeps its left hand to the wall and follows along.
+ */
+function WallFollower () {
+  this.dir = 's'
+}
+
+/**
+ * This method only has to 'scan' the critter's surroundings, starting from its left side and going
+ * clockwise until it finds an empty square. It then moves in the direction of that empty square.
+ *
+ * What complicates things is that a critter may end up in the middle of empty space,
+ * either as its start position or as a result of walking around another critter.
+ * To prevent the critter to keep on turning left at every step there is an extra check (the *if* statement)
+ * to start scanning to the left only if it looks like the critter has just passed some kind of obstacle ---
+ * that is, if the space behind and to the left of the critter is not empty. Otherwise, the critter starts
+ * scanning directly ahead, so that it will walk straight when in empty space.
+ *
+ * And finally, there's a test comparing *this.dir* to *start* after every pass through the loop to make
+ * sure that the loop won't run forever when the critter is walled in or crowded in by other critters and
+ * can't find an empty square.
+ */
+WallFollower.prototype.act = function (view) {
+  let start = this.dir
+  if (view.look(dirPlus(this.dir, -3)) !== ' ') {
+    start = this.dir = dirPlus(this.dir, -2)
+  }
+  while (view.look(this.dir) !== ' ') {
+    this.dir = dirPlus(this.dir, 1)
+    if (this.dir === start) {
+      break
+    }
+  }
+  return { type: 'move', direction: this.dir }
+}
+
+// --------------------------------------------------
+// Start displaying the world
+// --------------------------------------------------
+
+let world = new World(plan, { '#': Wall, 'o': BouncingCritter, '~': WallFollower })
 
 setInterval(function () {
   world.turn()
